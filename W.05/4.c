0968cf3e914a027cf504c874a929bfad9054896a
ZuuZ0 <urfavzuz@gmail.com>
2025-03-28 22:44:41 +0200

    W.05

--- W.05/4.c
+++ W.05/4.c
@@ -6,7 +6,7 @@ int bubble_sort(int *arr,int N){
     for(int i=0;i<N-1;i++){
     
         for(int j=0;j<N-i-1;j++){
-            if ( *(arr+j) < *(arr+j+1)){
+            if ( *(arr+j) > *(arr+j+1)){
     temp=*(arr+j);
     *(arr+j) = *(arr+j+1);
     *(arr+j+1) = temp;
